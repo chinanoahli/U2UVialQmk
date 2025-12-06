3a4a173193e3a7a88d6252a1a2da9d95767f8a0d
chinanoahli <10005128+chinanoahli@users.noreply.github.com>
2025-12-06 17:50:16 +0900

    Delay settings for outdated software

--- keyboards/sekigon/keyboard_quantizer/mini/config.h
+++ keyboards/sekigon/keyboard_quantizer/mini/config.h
@@ -22,3 +22,14 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+// My keyboard setting
+  // How long before a key press becomes a hold
+#define TAPPING_TERM 250
+  // Enables handling for per key `TAPPING_TERM` settings
+#define TAPPING_TERM_PER_KEY
+  // Specifically for outdated software !!
+  // Sets the delay between `register_code` and `unregister_code`, if you're having
+  // issues with it registering properly (common on VUSB boards). The value is
+  // in milliseconds and defaults to 0.
+#define TAP_CODE_DELAY 100
